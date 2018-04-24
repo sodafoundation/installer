7a341735f9b68bf8ca06c21ca6bb80765d7faefa
leonwanghui <wanghui71leon@gmail.com>
2018-04-24 12:37:17 +0800

    First verison of update
    Signed-off-by: leonwanghui <wanghui71leon@gmail.com>

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -20,4 +20,4 @@ sudo apt-get update
 sudo apt-get install -y ansible
 sleep 3
 
-ansible --version # Ansible version 2.4.2 or higher is required for ceph; 2.0.0.2 or higher is needed for other backends.
+ansible --version # Ansible version 2.4.2 or higher is required for ceph; 2.0.2 or higher is needed for other backends.

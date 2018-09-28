3e6a504dda767d0674a6b82f53c5197ec883e93b
lijuncloud <lijun166@huawei.com>
2018-09-28 10:35:19 +0800

    add copyright

--- ansible/global.sh
+++ ansible/global.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright (c) 2018 Huawei Technologies Co., Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## The global.sh file is created for setting global variables
+## you should set all variables you need in the script/vars.sh file 
+
 source ./script/vars.sh
 var_path=./group_vars
 

299219fd7a774c60b65031be282bcedbe0aa39e3
leonwanghui <wanghui71leon@gmail.com>
2019-05-07 11:22:02 +0800

    Improve ci test process
    Signed-off-by: leonwanghui <wanghui71leon@gmail.com>

---
+++ ci_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Copyright (c) 2019 The OpenSDS Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Validate the installation using release type
+sed -i 's/^install_from: .*/install_from: '"release"'/g' ansible/group_vars/common.yml
+sudo ansible-playbook ansible/site.yml -i ansible/local.hosts
+sudo ansible-playbook ansible/clean.yml -i ansible/local.hosts
+# Validate the installation using repository type
+sed -i 's/^install_from: .*/install_from: '"repository"'/g' ansible/group_vars/common.yml
+sudo ansible-playbook ansible/site.yml -i ansible/local.hosts
+sudo ansible-playbook ansible/clean.yml -i ansible/local.hosts
+# Validate the installation using container type
+sed -i 's/^install_from: .*/install_from: '"container"'/g' ansible/group_vars/common.yml
+sudo ansible-playbook ansible/site.yml -i ansible/local.hosts
+sudo ansible-playbook ansible/clean.yml -i ansible/local.hosts

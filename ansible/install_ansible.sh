ba401c3009a7854afbab5531a801d9ebced2c971
Nikhil Hari <nikhil_hari@live.com>
2020-05-24 08:39:39 +0530

    Install docker the official way

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -69,11 +69,21 @@ fi
 # enable universe repo
 echo Enabling universe repository.
 sudo add-apt-repository universe
+echo Enabling docker repositry
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common sshpass pv gawk
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo Installing ansible ${REQUIRED_ANSIBLE_VER} required for ceph-ansible ${CEPH_ANSIBLE_BRANCH} branch.
 sudo add-apt-repository -y ppa:ansible/ansible-${REQUIRED_ANSIBLE_VER}
 # Let universe and ansible repo update together
 sudo apt-get update
-sudo apt-get install -y sshpass pv gawk docker docker.io docker-compose ansible
+# Install docker and ansible dependencies
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io ansible
+# Installing docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 sleep 3
 sudo add-apt-repository -y -r ppa:ansible/ansible-${REQUIRED_ANSIBLE_VER}
 
